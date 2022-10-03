a27d191fcc234ceb4b627f67880df7affcf466fa
quoctan342 <quoctan342@gmail.com>
2022-10-03 16:11:26 +0700

    Add routes, view, sass/scss

---
+++ src/modules/books/views/create.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Create Book Page</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "create-book",
+});
+</script>
+
+<style scoped></style>
